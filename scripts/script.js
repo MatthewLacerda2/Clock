021bd578613acce262d3bdc5e143e01f624cfbef
lendacerda <math2.0@hotmail.com>
2023-03-24 12:40:47 -0300

    API :D

--- scripts/script.js
+++ scripts/script.js
@@ -5,19 +5,60 @@ function clock(){
 
     var hours=document.getElementById("hours").textContent=h;
     var minutes=document.getElementById("minutes").textContent=m;
-    var seconds=document.getElementById("seconds").textContent=s;
+    var seconds=document.getElementById("seconds").textContent=s;    
 
-    var d1= new Date().getDate();
-    var d2= new Date().getFullYear();
-    var dm= new Date().getMonth();
+    var d  =new Date().getDate();
+    var mes=new Date().getMonth();
+    var a  =new Date().getFullYear();    
 
-    var date1=document.getElementById("day").textContent=d1;
-    var datem=document.getElementById("month").textContent=dm;
-    var date2=document.getElementById("year").textContent=d2;
+    document.getElementById("day").textContent=d;
+    document.getElementById("month").textContent=mes;
+    document.getElementById("year").textContent=a;
 }
 
 var interval = setInterval(clock, 1000);
 
-function createCityClock(){
-    alert('debug')
+function createCityClock(value){
+    console.log(value)
+    
+    // Define your OpenWeatherMap API key and the base URL of the API
+    const API_KEY = "be66b4035e8f83af5b80982c259c116a";
+    const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+    // Define a function that takes a city name and returns a Promise that resolves to a JSON object
+    function getWeather(cityName) {
+        // Build the URL for the API request
+        const url = `${API_URL}?q=${cityName}&appid=${API_KEY}`;
+
+        // Fetch the data from the API and return it as JSON
+        return fetch(url)
+            .then(response => {
+            if (!response.ok) {
+                throw new Error(`Failed to fetch weather data: ${response.status}`);
+            }
+            return response.json();
+            })
+            .catch(error => {
+            console.error(error);
+            });
+    }
+
+    // Example usage
+    getWeather(value).then(data => {
+    console.log(data);
+    });
+
 }
+
+function seLivrar(){
+    alert("adios")
+}
+
+// var inputElement = document.getElementById("myInput");
+
+// inputElement.addEventListener("keypress", function(event) {
+//     if (event.keyCode === 13) {
+//         // Call your function here
+//         myFunction();
+//     }
+// });
