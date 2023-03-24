91207b299a3461ca92bcc7fed2cf96a74dea2712
lendacerda <math2.0@hotmail.com>
2023-03-24 00:21:39 -0300

    Just a clock

---
+++ script.js
@@ -0,0 +1,23 @@
+function clock(){
+    var h=new Date().getHours();
+    var m=new Date().getMinutes();
+    var s=new Date().getSeconds();
+
+    var hours=document.getElementById("hours").textContent=h;
+    var minutes=document.getElementById("minutes").textContent=m;
+    var seconds=document.getElementById("seconds").textContent=s;
+
+    var d1= new Date().getDate();
+    var d2= new Date().getFullYear();
+    var dm= new Date().getMonth();
+
+    var date1=document.getElementById("day").textContent=d1;
+    var datem=document.getElementById("month").textContent=dm;
+    var date2=document.getElementById("year").textContent=d2;
+}
+
+var interval = setInterval(clock, 1000);
+
+function createCityClock(){
+    alert('debug')
+}
